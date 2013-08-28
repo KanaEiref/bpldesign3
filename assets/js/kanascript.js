3aee025dbda0b338425c1c690d9696c1e20ff152
kana <kanaeiref@gmail.com>
2013-08-27 23:18:08 -0400

    first

---
+++ assets/js/kanascript.js
@@ -0,0 +1,119 @@
+var showServices = function() {
+	$('body').removeClass("active-left").toggleClass("active-right");
+	$('.hours-locations-button').removeClass("active-button");					
+	$('.service-button').toggleClass("active-button");
+};
+
+var showVisit = function() {
+	$('body').removeClass("active-right").toggleClass("active-left");
+	$('.service-button').removeClass("active-button");				
+	$('.hours-locations-button').toggleClass("active-button");	
+};
+
+// add/remove classes everytime the window resize event fires
+jQuery(window).resize(function(){
+	var off_canvas_nav_display = $('.off-canvas-navigation').css('display');
+	
+	if (off_canvas_nav_display === 'block') {			
+		$("body").removeClass("three-column").addClass("small-screen");				
+	} 
+	if (off_canvas_nav_display === 'none') {
+		$("body").removeClass("active-right active-left small-screen")
+			.addClass("three-column");			
+	}	
+	
+});	
+
+jQuery(document).ready(function($) {
+		// Toggle for left menu
+		$('.hours-locations-button').click(function(e) {
+			e.preventDefault();
+			showVisit();							
+		});	
+		// Toggle for right
+		$('.service-button').click(function(e) {
+			e.preventDefault();
+			showServices();									
+		});							
+});
+
+;(function() {
+
+	"use strict";
+
+	function clickmenuEvent(event){
+		$('#mobileMenu').toggleClass("onmenu");
+	}
+
+	function clickSearchEvent(event){
+
+
+		$('#searchbox').toggleClass("onmode");
+		$('#searchbox').focus();
+		$('#output').toggleClass("onoutput");
+	}
+
+	function searchEvent(event){
+		$.getJSON('assets/books.json', function(data){
+
+			var books = data.bookwarehouse,
+			count = books.length,
+			searchValue = $('input').val();
+
+			$('#output').empty();
+
+			if (count > 0 && searchValue != " "){
+
+				
+				$.each(books, function (i, obj) {
+				if (obj.name.indexOf(searchValue) != -1){
+					$('#output').append('<p> <a href="#">' +
+						obj.name + '&nbsp;&nbsp;&nbsp;&shy;&nbsp;' + obj.location + '</a><p>').hide().fadeIn();
+				}
+			});
+			}
+
+
+		}).error(function(){ alert('there was an ajax error');
+		}).complete(function(){}); //end of ajax call
+
+	}
+
+	$(function(){
+		$('#tiledMenu').data('size','big');
+	});
+
+	$(window).scroll(function()
+	{
+		if($(document).scrollTop() > 0)
+		{
+			if($('#tiledMenu').data('size') == 'big')
+			{		
+				$('#tiledMenu').data('size','small');
+				$('#tiledMenu').stop().animate({
+				height:'20px'
+				},300);
+			}
+		}
+		else
+		{
+			if($('#tiledMenu').data('size') == 'small')
+			{
+				$('#tiledMenu').data('size','big');
+				$('#tiledMenu').stop().animate({
+				height:'180px'
+				},300);
+			}
+		}
+	});
+
+	document.addEventListener("DOMContentLoaded", function(){
+		// var iconsearch = document.getElementById('iconsearch');
+		// iconsearch.addEventListener("click", clickSearchEvent, false);
+		// var searchbox = document.getElementById('searchbox');
+		// searchbox.addEventListener("keyup", searchEvent, false);
+		// var iconmenu = document.getElementById('iconmenu');
+		// iconmenu.addEventListener("click", clickmenuEvent, false);
+
+	}, false);	
+})();
